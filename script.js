732812c87a13b762c9f9e15abac799dcdab1087c
Ayomikun <96062920+teshyoma@users.noreply.github.com>
2023-01-17 16:43:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+$('#menu-btn').click(function() {
+    $('nav .navigation ul').addClass('active');
+});
+
+$('#menu-close').click(function() {
+    $('nav .navigation ul').removeClass('active');
+});
+
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
